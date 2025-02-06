fb1be1b012b202914226a7a6bfcf683692ed3b1a
pradeep <pradeep.s@thinkbridge.in>
2025-02-06 18:19:26 +0530

    Implement API handler for user management and update User model

--- flutter_webapplication/lib/models.dart
+++ flutter_webapplication/lib/models.dart
@@ -1,22 +1,29 @@
-class User{
-    final int userId;
-    final String name;
-    final String address;
+class User {
+  final int userId;
+  final String name;
+  final String address;
 
-    const User({
-        required this.userId,
-        required this.name,
-        required this.address});
+  const User({
+    required this.userId,
+    required this.name,
+    required this.address,
+  });
 
-    factory User.fromJson(Map<String, dynamic> json)=> User(
+  const User.empty({
+    this.userId = 0,
+    this.name = '',
+    this.address = '',
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
         userId: json['userId'],
         name: json['name'],
-        address: json['address']
-    );
+        address: json['address'],
+      );
 
-    Map<String, dynamic> toJson() => {
-        'userId': userId,
-        'name': name,
-        'address': address
-    };
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "name": name,
+        "address": address,
+      };
 }
